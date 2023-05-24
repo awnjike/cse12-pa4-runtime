78026f8e9d29f59e3f9a509b6d4b71c978e2d6fc
awnjike <130011672+awnjike@users.noreply.github.com>
2023-05-23 20:01:39 -0700

    updated
    goood

--- pa4-starter/pa4-student/src/cse12pa4student/Measure.java
+++ pa4-starter/pa4-student/src/cse12pa4student/Measure.java
@@ -5,10 +5,58 @@ import static cse12pa4mysteries.Mysteries.*;
 
 public class Measure {
 
-
+	List<Measurement> l = new List<Measurement>();
 	public static List<Measurement> measure(String[] toRun, int startN, int stopN) {
 		/** TODO **/
+		if(toRun.equals("A")){
+			long startTime = System.nanoTime();
+			int time = stopN - startN;
+			mysteryA(time);
+			long estimatedTime = System.nanoTime() - startTime;
+			Measurement x = Measurement(toRun, time ,estimatedTime);
+		}
+
+		if(toRun.equals("B")){
+			long startTime = System.nanoTime();
+			int time = stopN - startN;
+			mysteryB(time);
+			long estimatedTime = System.nanoTime() - startTime;
+			Measurement x = Measurement(toRun, time ,estimatedTime);
+		}
+
+		if(toRun.equals("C")){
+			long startTime = System.nanoTime();
+			int time = stopN - startN;
+			mysteryC(time);
+			long estimatedTime = System.nanoTime() - startTime;
+			Measurement x = Measurement(toRun, time ,estimatedTime);
+		}
 		
+		if(toRun.equals("D")){
+			long startTime = System.nanoTime();
+			int time = stopN - startN;
+			mysteryD(time);
+			long estimatedTime = System.nanoTime() - startTime;
+			Measurement x = Measurement(toRun, time ,estimatedTime);
+		}
+
+		if(toRun.equals("E")){
+			long startTime = System.nanoTime();
+			int time = stopN - startN;
+			mysteryE(time);
+			long estimatedTime = System.nanoTime() - startTime;
+			Measurement x = Measurement(toRun, time ,estimatedTime);
+		}
+
+		if(toRun.equals("F")){
+			long startTime = System.nanoTime();
+			int time = stopN - startN;
+			mysteryF(time);
+			long estimatedTime = System.nanoTime() - startTime;
+			Measurement x = Measurement(toRun, time ,estimatedTime);
+		}
+		list.add()
+
 		//Example call to mystery method
 		mysteryA(50);
 		return null;
